c7c30aa1fcce4ed56813c4e4ceec4dc32a90c303
Matt Berther <matt@berther.io>
2016-05-19 16:22:19 -0500

    migrating to mocha

--- test/simple.tests.js
+++ test/simple.tests.js
@@ -6,34 +6,23 @@
  *
  */
 
-var path = require('path'),
-    vows = require('vows'),
-    fs = require('fs'),
-    assert = require('assert'),
-    winston = require('winston'),
-    helpers = require('winston/test/helpers'),
-    DailyRotateFile = require('../');
+var path = require('path');
+var expect = require('chai').expect;
+var winston = require('winston');
+var rimraf = require('rimraf');
+var mkdirp = require('mkdirp');
+var MemoryStream = require('./memory-stream');
 
-
-function assertDailyRotateFile(transport) {
-  assert.instanceOf(transport, DailyRotateFile);
-  assert.isFunction(transport.log);
-};
-
-var transportTestSuite = require('winston/test/transports/transport');
+var DailyRotateFile = require('../');
 
 var fixturesDir = path.join(__dirname, 'fixtures');
-var stream = fs.createWriteStream(path.join(fixturesDir, 'testfile.log.2012-12-18'));
-var streamPrepended = fs.createWriteStream(path.join(fixturesDir, '2015-03-21_testfile.log'));
+rimraf.sync(fixturesDir);
+mkdirp(fixturesDir);
 
+// var stream = fs.createWriteStream(path.join(fixturesDir, 'testfile.log.2012-12-18'));
+// var streamPrepended = fs.createWriteStream(path.join(fixturesDir, '2015-03-21_testfile.log'));
+/*
 var transports = {
-  standard: new DailyRotateFile({
-    filename: path.join(fixturesDir, 'testfilename.log'),
-    datePattern: '.yyyy-MM-dd'
-  }),
-  failed: new DailyRotateFile({
-    filename: path.join(fixturesDir, 'dir404', 'testfile.log')
-  }),
   stream: new DailyRotateFile({ stream: stream }),
   prepended: new DailyRotateFile({
     filename: path.join(fixturesDir, 'testfilename.log'),
@@ -42,36 +31,100 @@ var transports = {
   }),
   streamPrepended: new DailyRotateFile({ stream: streamPrepended })
 };
+*/
 
-vows.describe('winston/transports/daily-rotate-file').addBatch({
-  "An instance of the Daily Rotate File Transport": {
-    "when passed a valid filename": {
-      "should have the proper methods defined": function () {
-        assertDailyRotateFile(transports.standard);
-      },
-      "the log() method": helpers.testNpmLevels(transports.standard, "should respond with true", function (ign, err, logged) {
-        assert.isNull(err);
-        assert.isTrue(logged);
-      })
-    },
-    "when passed an invalid filename": {
-      "should have proper methods defined": function () {
-        assertDailyRotateFile(transports.failed);
-      },
-      "should enter noop failed state": function () {
-        helpers.assertFailedTransport(transports.failed);
-      }
-    },
-    "when passed a valid file stream": {
-      "should have the proper methods defined": function () {
-        assertDailyRotateFile(transports.stream);
-      },
-      "the log() method": helpers.testNpmLevels(transports.stream, "should respond with true", function (ign, err, logged) {
-        assert.isNull(err);
-        assert.isTrue(logged);
-      })
-    }
-  },
+var transports = {
+  'file': new DailyRotateFile({
+    filename: path.join(fixturesDir, 'testfilename.log'),
+    datePattern: '.yyyy-MM-dd'
+  }),
+  'stream': new DailyRotateFile({stream: new MemoryStream()}),
+  'prepended file': new DailyRotateFile({
+    filename: path.join(fixturesDir, 'testfilename.log'),
+    datePattern: 'yyyy-MM-dd_',
+    prepend: true
+  })
+};
+
+describe('winston/transports/daily-rotate-file', function () {
+  describe('an instance of the transport', function () {
+    Object.keys(transports).forEach(function (t) {
+      describe('when passed a valid ' + t, function () {
+        var transport;
+
+        beforeEach(function () {
+          transport = transports[t];
+        });
+
+        it('should have the proper methods defined', function () {
+          expect(transport).to.be.instanceOf(DailyRotateFile);
+          expect(transport).to.respondTo('log');
+        });
+
+        var levels = winston.config.npm.levels;
+        Object.keys(levels).forEach(function (level) {
+          describe('with the ' + level + ' level', function () {
+            it('should respond with true when passed no metadata', function (done) {
+              transport.log(level, 'test message', {}, function (err, logged) {
+                expect(err).to.be.null;
+                expect(logged).to.be.true;
+                done();
+              });
+            });
+
+            var circular = { };
+            circular.metadata = circular;
+
+            var params = {
+              no: {},
+              object: {metadata: true},
+              primitive: 'metadata',
+              circular: circular
+            };
+
+            Object.keys(params).forEach(function (param) {
+              it('should respond with true when passed ' + param + ' metadata', function (done) {
+                transport.log(level, 'test message', params[param], function (err, logged) {
+                  expect(err).to.be.null;
+                  expect(logged).to.be.true;
+                  done();
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+
+    describe('when passed an invalid filename', function () {
+      var transport;
+
+      beforeEach(function () {
+        transport = new DailyRotateFile({
+          filename: path.join(fixturesDir, 'invalid', 'testfilename.log'),
+          datePattern: '.yyyy-MM-dd'
+        });
+      });
+
+      it('should have proper methods defined', function () {
+        expect(transport).to.be.instanceOf(DailyRotateFile);
+        expect(transport).to.respondTo('log');
+      });
+
+      it('should enter noop failed state', function (done) {
+        transport.on('error', function (emitErr) {
+          expect(emitErr).to.be.instanceOf(Error);
+          expect(emitErr.code, 'ENOENT');
+          expect(transport._failures).to.equal(transport.maxRetries);
+          done();
+        });
+
+        transport.log('error', 'test message');
+      });
+    });
+  });
+});
+/*
   "An instance of the Daily Rotate File Transport with 'prepend' option": {
    "when passed a valid filename": {
       "the log() method": helpers.testNpmLevels(transports.prepended, "should respond with true", function (ign, err, logged) {
@@ -106,3 +159,4 @@ vows.describe('winston/transports/daily-rotate-file').addBatch({
     prepend: true
   })
 }).export(module);
+*/
